e1f7a679ac7a6a3e22af390ecaec98f9a0002fc7
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-03-29 15:55:38 +0530

    -Demo set up

--- App.tsx
+++ App.tsx
@@ -1,36 +1,6 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, View, FlatList} from 'react-native';
-import Header from './src/components/header';
-import AddItem, {IItem} from './src/components/addItem';
-import Item from './src/components/item';
+import React from 'react';
+import RootNavigator from './src/navigation/navigationService';
 const App = () => {
-  const [shoppingList, setShoppingList] = useState<IItem[]>([]);
-  return (
-    <SafeAreaView style={styles.container}>
-      <Header title="Shopping List" />
-      <View style={styles.contentWrapper}>
-        <AddItem
-          setShoppingList={setShoppingList}
-          shoppingList={shoppingList}
-        />
-        <FlatList
-          data={shoppingList}
-          keyExtractor={(item, index) => `${item.item}-${index}`}
-          renderItem={({item}) => (
-            <Item item={item.item} quantity={item.quantity} />
-          )}
-        />
-      </View>
-    </SafeAreaView>
-  );
+  return <RootNavigator></RootNavigator>;
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e8e7e3',
-  },
-  contentWrapper: {
-    padding: 20,
-  },
-});
 export default App;

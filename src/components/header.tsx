1e2df2a04f0d415e5b0058a4dd5f33ff08ae47c8
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-03-30 18:11:15 +0530

    Typescript navigation set up

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  GestureResponderEvent,
+} from 'react-native';
+import DrawerMenuIcon from '../assets/icons/drawerMenuIcon';
 interface Props {
   title: string;
+  menuPress?: (event: GestureResponderEvent) => void;
 }
-const Header: React.FC<Props> = ({title}) => {
+const Header: React.FC<Props> = ({title, menuPress}) => {
   return (
     <View style={styles.header}>
       <Text style={styles.headerText}>{title}</Text>
+      <TouchableOpacity style={styles.menuStyle} onPress={menuPress}>
+        <DrawerMenuIcon color="#234" size={30} />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -16,10 +27,19 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#cfcfcf',
     alignItems: 'center',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'center',
   },
   headerText: {
     fontSize: 20,
-    fontWeight: '600',
+    fontWeight: 'bold',
+  },
+  menuStyle: {
+    position: 'absolute',
+    left: 0,
+    padding: 10,
+    // alignSelf: 'flex-start',
   },
 });
 export default Header;

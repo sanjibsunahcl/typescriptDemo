65bbfd689d1eba810e4041516a6d3fbfe004f3f6
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-03-30 18:15:33 +0530

    Typescript navigation set up

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import DrawerNavigation from './src/navigation/drawerNavigator';
+import 'react-native-gesture-handler';
 import RootNavigator from './src/navigation/navigationService';
 const App = () => {
-  return <RootNavigator></RootNavigator>;
+  return <DrawerNavigation></DrawerNavigation>;
 };
 export default App;

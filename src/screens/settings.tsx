f592119b28f0cbb45e6509fe9c8d5a87c944b356
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-04-05 10:50:51 +0530

    redux set up

--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -1,31 +1,96 @@
-import * as React from 'react';
-import {View, Button, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from './home';
 import Header from '../components/header';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {ActionCreators} from '../redux/actions';
 
 type SettingScreenProps = NativeStackScreenProps<
   RootStackParamList,
   'settings'
 >;
 
-const SettingScreen: React.FC<SettingScreenProps> = props => {
+interface apiData {
+  title: string;
+  releaseYear: string;
+  image: string;
+}
+
+const SettingScreen: React.FC<SettingScreenProps> = (props: any) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    props.geApiDataAction();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const {responseData} = props;
+    // console.log('item' + JSON.stringify(responseData));
+    if (responseData) {
+      setData(responseData.movies);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.responseData]);
+
+  const renderMovieList = (item: apiData, index: number) => {
+    // console.log('item' + JSON.stringify(item));
+    return (
+      <TouchableOpacity
+        style={styles.itemContainer}
+        onPress={() =>
+          Alert.alert(JSON.stringify(item), JSON.stringify(index))
+        }>
+        <Text style={{fontSize: 20}}>
+          {item.title}, {item.releaseYear}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
   return (
     <View style={styles.container}>
       <Header
-        title="Setting Screen"
+        title="Listing Screen"
         menuPress={() => props.navigation.openDrawer()}
       />
-      <Text>Settings Screen</Text>
-      {/*   <Button
-        title="Go to Profile"
-        onPress={() => props.navigation.push('profile')}
-      /> */}
+      <View style={styles.subContainer}>
+        {props.isLoading ? (
+          <ActivityIndicator size={'large'} style={{alignSelf: 'center'}} />
+        ) : (
+          <FlatList
+            data={data}
+            // style={{height: 100}}
+            keyExtractor={({id}) => id}
+            renderItem={({item, index}) => renderMovieList(item, index)}
+          />
+        )}
+      </View>
     </View>
   );
 };
 
-export default SettingScreen;
+const mapStateToProps = (state: any) => {
+  console.log('responseData nnn' + JSON.stringify(state));
+  return {
+    responseData: state.demo.responseData,
+    isLoading: state.demo.isLoading,
+  };
+};
+function mapDispatchToProps(dispatch: any) {
+  return bindActionCreators(ActionCreators, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SettingScreen);
 
 const styles = StyleSheet.create({
   container: {
@@ -33,5 +98,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     // justifyContent: 'center',
     backgroundColor: 'white',
+  },
+  itemContainer: {
+    marginTop: 10,
+    borderColor: '#234',
+    borderWidth: 1,
+    padding: 15,
+    borderRadius: 5,
+  },
+  subContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    width: 350,
   },
 });
